70dcca0c89462cbdc536d91ed1e695c49fb340c5
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2020-07-31 13:24:32 -0400

    Added Links and Live Demos for Projects

---
+++ SpaceCadets/JS/navbar.js
@@ -0,0 +1,23 @@
+var navbarcontent = '<nav><div id="navbackground"><ul><li id="sandwichicon"><img src="Assets/Icons/menu.svg"></li><li id="navtitle">Template</li><div class="borderXwidth"><li><a class="tab" id ="filledtab" href="tab4.html">Log In</a></li><li><a class="tab" id="outlinetab" href="tab3.html">Sign Up</a></li><li><a class="tab" href="tab2.html">Tab 2</a></li><li><a class="tab" href="tab1.html">Tab 1</a></li></div></ul></div></nav>'
+
+document.write(navbarcontent);
+
+/*
+Navbar content:
+<nav>
+            <div id="navbackground">
+                <ul>
+                    <li id="sandwichicon"><img src="Assets/Icons/menu.svg"></li>
+                    <li id="navtitle">Template</li>
+                    <div class="borderXwidth">
+                    <li><a class="tab" href="tab4.html">Tab 4</a></li>
+                    <li><a class="tab" href="tab3.html">Tab 3</a></li>
+                    <li><a class="tab" href="tab2.html">Tab 2</a></li>
+                    <li><a class="tab" href="tab1.html">Tab 1</a></li>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+
+
+*/
---
+++ SpaceCadets/README.md
@@ -0,0 +1,3 @@
+#SpaceCadetsTemplate
+To see the full github, go here:
+https://github.com/varunan-vara/SpaceCadetTemplate
