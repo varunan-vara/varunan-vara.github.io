e6389b827d8b9b6855dda97f9df62a718a005f05
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2022-01-27 01:22:21 -0500

    Lightened CSS, added Title page

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-import './style.css'
-
 import * as THREE from 'three';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
-const loader = new OBJLoader();
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 10000);
 const renderer = new THREE.WebGL1Renderer({
     canvas: document.querySelector("#background"),
@@ -16,6 +12,12 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+function resizehandler () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener( "resize", resizehandler, false);
 const controls = new OrbitControls(camera, renderer.domElement)
 
 /* -----Render Title Card----- */
@@ -57,8 +59,9 @@ function randomgreen (min, max) {
     return (min + (Math.random() * (max - min))) / 255
 }
 
+
 function icosahedron () {
-    const funky = new THREE.IcosahedronGeometry((Math.random() * 10), 1);
+    const funky = new THREE.IcosahedronGeometry((Math.random() * 5), 1);
     const material = new THREE.MeshStandardMaterial({});
     material.color.setRGB(randomgreen(29,103), randomgreen(120,146), randomgreen(116,137));
     const wireframemesh = new THREE.MeshBasicMaterial({
@@ -73,28 +76,80 @@ function icosahedron () {
 }
 
 function flyingshape () {
-    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(475));
+    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(700));
     var mesh = icosahedron();
     mesh.position.set(x,y,z);
     return mesh
 }
 
+function roundedrect (l, w, r) {
+    const rrect = new THREE.Shape();
+    rrect.moveTo(0,r);
+    rrect.lineTo(0, l-r);
+    rrect.bezierCurveTo(0, l-r, l, 0, l, r);
+    rrect.lineTo(w-r, l);
+    rrect.bezierCurveTo(w-r,l,w,l,w,l-r);
+    rrect.lineTo(w, r);
+    rrect.bezierCurveTo(w,r,w,0,w-r,0)
+    rrect.lineTo(r,0)
+    rrect.bezierCurveTo(r,0,0,0,0,r);
+    return rrect
+}
+
+function plaque () {
+    const extrudesettings = {
+        steps: 2,
+        depth: 2,
+    }
+    const geometry = new THREE.ExtrudeGeometry(roundedrect(40,30,2),extrudesettings);
+    const meshmaterial = new THREE.MeshStandardMaterial({});
+    meshmaterial.color.set(0xFEFEFE);
+    const wireframemesh = new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        wireframe: true,
+        transparent : true
+    });
+    const mesh = new THREE.mesh(geometry, meshmaterial);
+    const wireframe = new THREE.mesh(geometry, wireframemesh);
+    mesh.add(wireframe);
+    return mesh
+}
+
+function scroller () {
+    const t = document.body.getBoundingClientRect().top;
+
+    camera.position.y = (t) * -0.001;
+    camera.position.x = (t) * -0.005;
+}
+
 function animate() {
     requestAnimationFrame( animate );
     controls.update();
-    for (var i = 0; i < 100; i++) {
-        objects[i].rotation.x += 0.001;
-        objects[i].rotation.y += 0.005;
-        objects[i].rotation.z += 0.004;
-
+    for (var i = 0; i < 400; i++) {
+        objects[i].rotation.x += Math.random() * 0.005;
+        objects[i].rotation.y += Math.random() * 0.001;
+        objects[i].rotation.z += Math.random() * 0.007;
     }
+    console.log(window.innerWidth);
     renderer.render( scene, camera );
+
+    //temp
+    camera.updateMatrix();
+    var vector = camera.position.clone();
+    //vector.applyMatrix3( camera.matrixWorld );
+    console.log(vector)
 }
 
-
-const objects = Array(100).fill().map(flyingshape);
-for (var i = 0; i < 100; i++) {
+const objects = Array(5000).fill().map(flyingshape);
+for (var i = 0; i < 400; i++) {
     scene.add(objects[i])
 }
+
+document.body.onscroll = scroller;
 lightup();
+
+//unused
+roundedplaque = plaque();
+
 animate();
+
